bcca9e2612d788929e5ecc29165d4307a0329bff
Pablo Lichtig <m300788@levante6.lvt.dkrz.de>
2024-01-27 17:47:31 +0100

    finish the point selection

--- vertical_crossection.py
+++ vertical_crossection.py
@@ -14,21 +14,30 @@ def linear_equation_from_points(lat_init, lat_end, lon_init, lon_end):
     """
     Find the linear equation from two points.
     lat = m * lon + lat_init
+    returns slope, intercept
     """
 
+    if lon_init > lon_end:
+        raise Exception("Initial longitude should be less or equal than final longitude.")
+    if lat_init > lat_end:
+        raise Exception("Initial latitude should be less or equal than final latitude.")
+    if (lon_init == lon_end) and (lat_init == lat_end):
+        raise Exception("Initial and final coordinates are the same. No line is possible.")
     if lon_init == lon_end:
-        return "vertical line, no equation possible"
+        raise Exception("vertical line, no equation possible")
     if lat_init == lat_end:
         print("Horizontal line, is a constant.")
-        return {"slope": 0, "intercept": lat_init}
+        slope, intercept = 0, lat_init
+        return slope, intercept
     slope = (lat_end - lat_init)/(lon_end - lon_init)
     if lon_init == 0:
         intercept = lat_init
     elif lon_end == 0:
         intercept = lat_end
     else:
-        intercept = lat_init / (slope * lon_init)
-    return {"slope": slope, "intercept": intercept}
+        intercept = lat_init - (slope * lon_init)
+    print(slope, intercept)
+    return slope, intercept
 
 def cross_section_points(lat_init, lat_end, lon_init, lon_end, step):
     """
@@ -45,17 +54,14 @@ def cross_section_points(lat_init, lat_end, lon_init, lon_end, step):
         lats = np.arange(lat_init, lat_end+step, step)
         lons = np.ones(len(lats)) * lon_init
     else:
-        return "The function isn't ready yet for a diagonal cross section.\nComing soon!"
+        slope, intercept = linear_equation_from_points(
+            lat_init=lat_init, lon_init=lon_init,
+            lat_end=lat_end, lon_end=lon_end
+        )
+        lons = np.arange(lon_init, lon_end+step, step)
+        lats = slope * lons + intercept
 
     print("lats", lats)
     print("lons", lons)
     lonlat = np.stack((lons, lats), axis=1)
     return lonlat
-
-def main():
-    test = cross_section_points(-10, 10, -50, -50, 1)
-    display(test)
-
-#! =================================================
-if __name__ == "__main__":
-    main()
